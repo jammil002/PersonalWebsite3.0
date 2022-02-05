aa219d20d7b87bd64976fa76f302bd15be820f74
James Miller <jam1211miller@gmail.com>
2022-02-05 16:16:26 -0700

    Starting lazy load

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,17 +1,5 @@
 const sectionOne = document.querySelector('.logo');
 const sectionTwo = document.querySelector('.sectionOne');
 const sectionThree = document.querySelector('.sectionTwo');
+const sectionThree = document.querySelector('.sectionThree');
 const sectionFour = document.querySelector('.final__section');
-
-const options = {
-    root: null,
-    threshold: 0.25,
-    rootMargin: "-150px",
-};
-
-const observer =
-    new IntersectionObserver(function (entries, obererver) {
-        entries.forEach(entry => {
-            console.log(entry.target);
-        });
-    }, options);
