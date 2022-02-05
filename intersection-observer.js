f87d7dcb90cae67f1741735c47be4085fafe3668
James Miller <jam1211miller@gmail.com>
2022-02-05 16:22:05 -0700

    Changed javascript

--- intersection-observer.js
+++
@@ -1,5 +0,0 @@
-const sectionOne = document.querySelector('.logo');
-const sectionTwo = document.querySelector('.sectionOne');
-const sectionThree = document.querySelector('.sectionTwo');
-const sectionThree = document.querySelector('.sectionThree');
-const sectionFour = document.querySelector('.final__section');
